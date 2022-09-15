94842c9700c575235d9e9ee4412813d107f8e4b0
zubaidmaulana <113624156+zubaidmaulana@users.noreply.github.com>
2022-09-15 23:07:12 +0700

    Add files via upload

---
+++ BALOK DAN KUBUS ZUBAID PY.py
@@ -0,0 +1,26 @@
+# PROGRAM MENGHITUNG VOLUME BALOK PADA PHYTON VERSI DINAMIS
+print("PROGRAM MENGHITUNG VOLUME BALOK")
+p = float(input("nilai panjang = "))
+l = float(input("nilai lebar = "))
+t = float(input("nilai tinggi = "))
+v = p*l*t
+print ("volume balok =",v)
+
+# PROGRAM MENGHITUNG VOLUME BALOK PADA PHYTON VERSI STATIS
+p = 6
+l = 3
+t = 4
+v = p*l*t
+print ("volume balok =",v)
+
+# PROGRAM MENGHITUNG VOLUME KUBUS PADA PHYTON VERSI DINAMIS
+print("PROGRAM MENGHITUNG VOLUME KUBUS")
+s = float(input("nilai sisi"))
+v = s*s*s*s*s
+print ("volume kubus",v)
+
+# PROGRAM MENGHITUNG VOLUME KUBUS PADA PHYTON VERSI STATIS
+print("PROGRAM MENGHITUNG VOLUME KUBUS")
+s = 25
+v = s*s*s*s*s
+print ("volume kubus",v)
